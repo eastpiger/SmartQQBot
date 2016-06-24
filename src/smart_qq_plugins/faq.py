3e6b18941e49509495a625890a47fd3d14ad6900
eastpiger <lvwt@shanghaitech.edu.cn>
2016-06-25 00:49:10 +0800

    change plugins

--- src/smart_qq_plugins/faq.py
+++ src/smart_qq_plugins/faq.py
@@ -3,11 +3,15 @@ import json
 import urllib
 import logging
 import time
+import datetime
 import os
 import random
 from random import randint
 import re
 from smart_qq_bot.signals import on_group_message, on_private_message
+
+pretime = [datetime.datetime.min(), datetime.datetime.min()]
+delta = datetime.timedelta(minutes=30)
 
 ##过滤HTML中的标签
 #将HTML中标签等信息去掉
@@ -128,27 +132,93 @@ def send_msg(msg, bot):
     :type bot: smart_qq_bot.bot.QQBot
     :type msg: smart_qq_bot.messages.GroupMsg
     """
-    if bot.gid_to_name(msg.from_uin) == '好好学习天天向上':
-        t = try_faq(msg.content, bot)
-        if t:
-            t = filter_tags(t)
-            t += '\n----\n{}我是上科大GeekPie机器人。加好友私聊提供更多信息问答服务。'.format(maimeng())
-            while len(t) > 400:
-                bot.reply_msg(msg, t[:400])
-                t = t[400:]
-                time.sleep(3)
-            bot.reply_msg(msg, t)
-    if bot.gid_to_name(msg.from_uin) in ('上科大本招咨询①群', '上科大本招咨询②群'):
-        t = try_faq(msg.content, bot)
-        if t:
-            t = filter_tags(t)
-            t += '\n----\n{}我是上科大GeekPie机器人。加好友私聊提供更多信息问答服务。'.format(maimeng())
-            while len(t) > 400:
-                bot.reply_msg(msg, t[:400])
-                t = t[400:]
-                time.sleep(3)
+#     if bot.gid_to_name(msg.from_uin) == '好好学习天天向上':
+#         nowtime = datetime.datetime.now()
+#         timedelta = nowtime - pretime[0]
+#         if timedelta >= delta:
+#             pretime[0] = nowtime
+#             t += '''1.  上海考生须知：
+# 各位上海同学、家长：如果对综评有纠结的可以打电话到4009209393或者18930933479，报上考生的情况，了解是否有录取的可能。请报出完整信息，包括姓名、高考裸分和加分。这三个都要报的。我们会针对个人给出相应信息。分数线现在肯定出不来，因为还没到最后投档的时候，请各位知悉。电话的人会比较多，如果占线请稍候再试试，也请打电话简明扼要，不要影响其他人打进来，谢谢！
+
+# 2.  外省市考生须知：
+# （1） 请问在X X省的招生计划数是多少？
+# 请查看您所在省份的招生计划书（俗称“大厚本”），在提前批次可以找到我校的招生计划数。各地招生计划数、历年分数线、具体录取流程请咨询当地本科招生小组，或参考各高中统订统发的由地方考试院出版的《招生目录》或《志愿填报手册》等出版物。
+
+# （2）高考出分后请及时反馈高考成绩（江苏省不需要反馈）：http://admission.shanghaitech.edu.cn/score_2016.php
+
+# （3）如有问题请及时与我校在的当地本科招生小组取得联系，联系方式表http://asa.shanghaitech.edu.cn/admission_detail.asp?id=136
+# 各地招生计划数、历年分数线、具体录取流程及其他报考问题，均可直接拨打当地招生小组的电话咨询，仅在招生群内咨询没有办法回答。另外，请注意在开通时间内咨询。
+
+# 重要提示：除江苏省有特殊政策，需要获得开放日加分方可报考外，全国其他地区考生可在提前批次第一志愿填报我校。上科大录取按高考分+校园开放日加分后的总分降序排序，择优录取。
+
+# [我是机器人，不要调戏我，谢谢~{}]
+# '''.format(maimeng())
+#             bot.reply_msg(msg, t)
+        # t = try_faq(msg.content, bot)
+        # if t:
+        #     t = filter_tags(t)
+        #     t += '\n----\n{}我是上科大GeekPie机器人。加好友私聊提供更多信息问答服务。'.format(maimeng())
+        #     while len(t) > 400:
+        #         bot.reply_msg(msg, t[:400])
+        #         t = t[400:]
+        #         time.sleep(3)
+        #     bot.reply_msg(msg, t)
+    # if bot.gid_to_name(msg.from_uin) in ('上科大本招咨询①群', '上科大本招咨询②群'):
+    #     t = try_faq(msg.content, bot)
+    #     if t:
+    #         t = filter_tags(t)
+    #         t += '\n----\n{}我是上科大GeekPie机器人。加好友私聊提供更多信息问答服务。'.format(maimeng())
+    #         while len(t) > 400:
+    #             bot.reply_msg(msg, t[:400])
+    #             t = t[400:]
+    #             time.sleep(3)
+    #         bot.reply_msg(msg, t)
+
+    if bot.gid_to_name(msg.from_uin) == '上科大本招咨询①群':
+        nowtime = datetime.datetime.now()
+        timedelta = nowtime - pretime[0]
+        if timedelta >= delta:
+            pretime[0] = nowtime
+            t += '''1.  上海考生须知：
+各位上海同学、家长：如果对综评有纠结的可以打电话到4009209393或者18930933479，报上考生的情况，了解是否有录取的可能。请报出完整信息，包括姓名、高考裸分和加分。这三个都要报的。我们会针对个人给出相应信息。分数线现在肯定出不来，因为还没到最后投档的时候，请各位知悉。电话的人会比较多，如果占线请稍候再试试，也请打电话简明扼要，不要影响其他人打进来，谢谢！
+
+2.  外省市考生须知：
+（1） 请问在X X省的招生计划数是多少？
+请查看您所在省份的招生计划书（俗称“大厚本”），在提前批次可以找到我校的招生计划数。各地招生计划数、历年分数线、具体录取流程请咨询当地本科招生小组，或参考各高中统订统发的由地方考试院出版的《招生目录》或《志愿填报手册》等出版物。
+
+（2）高考出分后请及时反馈高考成绩（江苏省不需要反馈）：http://admission.shanghaitech.edu.cn/score_2016.php
+
+（3）如有问题请及时与我校在的当地本科招生小组取得联系，联系方式表http://asa.shanghaitech.edu.cn/admission_detail.asp?id=136
+各地招生计划数、历年分数线、具体录取流程及其他报考问题，均可直接拨打当地招生小组的电话咨询，仅在招生群内咨询没有办法回答。另外，请注意在开通时间内咨询。
+
+重要提示：除江苏省有特殊政策，需要获得开放日加分方可报考外，全国其他地区考生可在提前批次第一志愿填报我校。上科大录取按高考分+校园开放日加分后的总分降序排序，择优录取。
+
+[我是机器人，不要调戏我，谢谢~{}]
+'''.format(maimeng())
             bot.reply_msg(msg, t)
 
+    if bot.gid_to_name(msg.from_uin) == '上科大本招咨询②群':
+        nowtime = datetime.datetime.now()
+        timedelta = nowtime - pretime[1]
+        if timedelta >= delta:
+            pretime[1] = nowtime
+            t += '''1.  上海考生须知：
+各位上海同学、家长：如果对综评有纠结的可以打电话到4009209393或者18930933479，报上考生的情况，了解是否有录取的可能。请报出完整信息，包括姓名、高考裸分和加分。这三个都要报的。我们会针对个人给出相应信息。分数线现在肯定出不来，因为还没到最后投档的时候，请各位知悉。电话的人会比较多，如果占线请稍候再试试，也请打电话简明扼要，不要影响其他人打进来，谢谢！
+
+2.  外省市考生须知：
+（1） 请问在X X省的招生计划数是多少？
+请查看您所在省份的招生计划书（俗称“大厚本”），在提前批次可以找到我校的招生计划数。各地招生计划数、历年分数线、具体录取流程请咨询当地本科招生小组，或参考各高中统订统发的由地方考试院出版的《招生目录》或《志愿填报手册》等出版物。
+
+（2）高考出分后请及时反馈高考成绩（江苏省不需要反馈）：http://admission.shanghaitech.edu.cn/score_2016.php
+
+（3）如有问题请及时与我校在的当地本科招生小组取得联系，联系方式表http://asa.shanghaitech.edu.cn/admission_detail.asp?id=136
+各地招生计划数、历年分数线、具体录取流程及其他报考问题，均可直接拨打当地招生小组的电话咨询，仅在招生群内咨询没有办法回答。另外，请注意在开通时间内咨询。
+
+重要提示：除江苏省有特殊政策，需要获得开放日加分方可报考外，全国其他地区考生可在提前批次第一志愿填报我校。上科大录取按高考分+校园开放日加分后的总分降序排序，择优录取。
+
+[我是机器人，不要调戏我，谢谢~{}]
+'''.format(maimeng())
+            bot.reply_msg(msg, t)
 
 @on_private_message(name="faq")
 def remove(msg, bot):
@@ -156,8 +226,8 @@ def remove(msg, bot):
     :type bot: smart_qq_bot.bot.QQBot
     :type msg: smart_qq_bot.messages.GroupMsg
     """
-    t = try_faq_p(msg.content, bot)
-    if t:
-        t = filter_tags(t)
-        t += '\n----\n{}我是上科大GeekPie机器人。加好友私聊提供更多信息问答服务。'.format(maimeng())
-        bot.reply_msg(msg, t)
+    # t = try_faq_p(msg.content, bot)
+    # if t:
+    #     t = filter_tags(t)
+    #     t += '\n----\n{}我是上科大GeekPie机器人。加好友私聊提供更多信息问答服务。'.format(maimeng())
+    #     bot.reply_msg(msg, t)
